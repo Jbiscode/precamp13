69e0c142e695230e0c53cdf764093f0ee7701aa2
Jbiscode <code6995@gmail.com>
2022-02-16 17:52:36 +0900

    test

---
+++ 연습문제/1변수와상수.js
@@ -0,0 +1,32 @@
+// //001. 선언과 할당     ///        
+// const name = "재빈"
+// console.log(name)
+
+
+
+// //002. 재할당         ////
+// let name = "재빈이"
+// console.log(name)
+// name = "철수"
+// console.log(name)
+
+
+
+// //003. 배열의 선언과 할당    ////
+// fruits = ['사과','바나나','파인애플']
+// console.log(fruits)
+
+
+
+// //004. 배열의 기능    ////
+// fruits = ['사과','바나나','파인애플']
+// console.log(fruits)
+// console.log(fruits.length)
+// newFruits=[]
+// newFruits.push(fruits[fruits.length-1])
+// console.log(newFruits)
+
+
+
+//005. 배열의 기능    //
+let students = ["철수", "영희", "훈이", "짱구", "유리"]
